88413bc09d5e1002f23a3e416a87175fc6186355
Jhonatan Sarmento <jhonatan.sarmento@watch.tv.br>
2024-08-08 21:49:00 -0400

    feat: set edge runtime and preferred region for HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,3 +3,6 @@ import ProductList from './components/ProductList/ProductList';
 export default function HomePage() {
   return <ProductList />;
 }
+
+export const runtime = 'edge';
+export const preferredRegion = 'iad1';
--- src/services/fetchProducts.ts
+++ src/services/fetchProducts.ts
@@ -1,4 +1,4 @@
-import { Product } from '../types/Product';
+import { Product } from '../app/components/ProductList/types';
 
 export const fetchProducts = async (): Promise<Product[]> => {
   try {
